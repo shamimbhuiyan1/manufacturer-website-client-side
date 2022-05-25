1904fc665109f630ad6375344108e3113757f846
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-05-25 17:34:48 +0600

    not found

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home/Home";
+import NotFound from "./Pages/NotFound/NotFound";
 import Portfolio from "./Pages/Portfolio/Portfolio";
 import Footer from "./Shared/Footer/Footer";
 import Navbar from "./Shared/Navbar/Navbar";
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/portfolio" element={<Portfolio></Portfolio>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+import notfound from "../../assets/not-found.webp";
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className="text-center font-bold my-6 text-4xl text-red-500">
+        Opp'S !!! Page Not Found.
+      </h1>
+      <img src={notfound} alt="not found page" />
+    </div>
+  );
+};
+
+export default NotFound;
